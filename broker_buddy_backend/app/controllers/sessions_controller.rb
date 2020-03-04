a2cfdd5624e635c93224c833b60ee95a764c1412
dean <deanlloyd64@gmail.com>
2020-03-04 13:25:51 -0500

    create test session logic

--- broker_buddy_backend/app/controllers/sessions_controller.rb
+++ broker_buddy_backend/app/controllers/sessions_controller.rb
@@ -1,11 +1,17 @@
 class SessionsController < ApplicationController
 
     def create
+        user = User.find_by(email: params[:email])
 
+        if !!user && user.valid_password?(params[:password])
+            render json: user.as_json(only: [:email]), status: :created
+        else
+            head(:unauthorized)
+        end
     end
 
     def destroy
-        
+
     end
 
 end
