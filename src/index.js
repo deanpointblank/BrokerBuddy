27106d8d61385b0d51b55d13cf90149d889ab543
dean <deanlloyd64@gmail.com>
2020-03-04 22:16:23 -0500

    add placeholder reducer. add code for thun and react-dev-tools

--- src/index.js
+++ src/index.js
@@ -4,7 +4,39 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { Proivider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk'
+
+import { allReducers } from './reducers/index'
+
+const store = createStore(null, applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
