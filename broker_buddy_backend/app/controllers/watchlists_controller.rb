ec502398017ad2b69da44b8018ca880b2ae82ba2
dean <deanlloyd64@gmail.com>
2020-03-04 21:23:47 -0500

    rewrite watchlist actions to proper controller

--- broker_buddy_backend/app/controllers/watchlists_controller.rb
+++ broker_buddy_backend/app/controllers/watchlists_controller.rb
@@ -1,2 +1,38 @@
 class WatchlistsController < ApplicationController
+
+    def index
+        watchlists = Watchlist.all
+
+        render json: watchlists.as_json
+    end
+
+
+    def create
+        watchlist = Watchlist.create(name: params[:name])
+    
+        if watchlist.save
+          render json: watchlist.as_json, status: :created
+        else
+          head(:unauthorized)
+        end
+    
+      end
+    
+      def update
+        watchlist = Watchlist.find(id: params[:id])
+        if !!watchlist && watchlist.update(stocks: params[:stocks])
+    
+          render json: watchlist.as_json
+        else
+          head(:unauthorized)
+        end
+      end
+    
+      def destroy
+        watchlist = Watchlist.find(id: params[:id])
+        watchlist.destroy
+    
+      end
+
+
 end
--- broker_buddy_backend/app/models/watchlist.rb
+++ broker_buddy_backend/app/models/watchlist.rb
@@ -1,16 +1,4 @@
 class Watchlist < ApplicationRecord
   belongs_to :user
 
-  def index
-  end
-
-  def create
-  end
-
-  def update
-  end
-
-  def destroy
-  end
-
 end
