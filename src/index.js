519306e9ab9a5b003845fefa068d54a0b743da8a
dean <deanlloyd64@gmail.com>
2020-03-05 16:40:45 -0500

    add react-router

--- src/index.js
+++ src/index.js
@@ -4,16 +4,20 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import { Proivider } from 'react-redux';
+import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk'
 
-import { allReducers } from './reducers/index'
+import { allReducers } from './Reducers/index'
 
 const store = createStore(null, applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root'));
 
 
 
