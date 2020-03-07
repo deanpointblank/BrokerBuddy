e64377cbe1100c94fa29021fcf4c4ca626bf2058
dean <deanlloyd64@gmail.com>
2020-03-07 01:02:55 -0500

    create controlled registration form

--- src/Containers/LogInRegister/Register.js
+++ src/Containers/LogInRegister/Register.js
@@ -1,21 +1,43 @@
 import React, { Component } from 'react';
 
 class Register extends Component{
+    constructor(){
+        super()
+        this.state = {
+            email: '',
+            firstName: '',
+            lastName: '',
+            password: '',
+            confirmedPassword: ''
+        }
+    }
+
+    handleOnChange(event){
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleOnSubmit(event){
+        event.preventDefault()
+        console.log("Submit!")
+    }
+
     render(){
         return(
-            <form>
+            <form onSubmit={event => this.handleOnSubmit(event)}>
                 <label>Email</label>
-                <input type='text' />
+                <input type='text' name='email' value={this.state.email} onChange={event => this.handleOnChange(event)}/>
                 <br />
                 <label>First Name</label>
-                <input type='text' />
+                <input type='text' name='firstName' value={this.state.firstName} onChange={event => this.handleOnChange(event)}/>
                 <label>Last Name</label>
-                <input type='text' />
+                <input type='text' name='lastName' value={this.state.lastName} onChange={event => this.handleOnChange(event)}/>
                 <br/>
                 <label>Password</label>
-                <input type='text' />
+                <input type='text' name='password' value={this.state.password} onChange={event => this.handleOnChange(event)}/>
                 <label>Comfirm Password</label>
-                <input type='text' />
+                <input type='text' name='confirmedPassword' value={this.state.confirmedPassword} onChange={event => this.handleOnChange(event)}/>
                 <br />
                 <input type='submit' />
             </form>
