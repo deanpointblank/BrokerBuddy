987c2479413f87a1afa0d5130a59780fd7e2da34
dean <deanlloyd64@gmail.com>
2020-03-08 00:19:19 -0500

    fix bug where user token isn't sent back to client

--- broker_buddy_backend/app/controllers/sessions_controller.rb
+++ broker_buddy_backend/app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     def create
         user = User.find_by(email: params[:email])
 
-        if !!user && user.valid_password?(params[:password])
+        if user.save && user.valid_password?(params[:password])
             render json: user.as_json(only: [:email, :authentication_token]), status: :created
         else
             head(:unauthorized)
