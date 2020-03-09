335bf7ebf3abefe75c71fdd21f3abbcc8344abb2
dean <deanlloyd64@gmail.com>
2020-03-09 16:13:52 -0400

    create register reducer to track registration status

---
+++ src/Reducers/RegisterReducer.js
@@ -0,0 +1,27 @@
+const RegisterReducer = (state = {
+    isRegisterSuccess: false,
+    isRegisterPending: false,
+    isRegisternError: null
+}, action) => {
+    switch(action.type){
+        case 'SET_REGISTER_PENDING':
+            return {
+                ...state,
+                isRegisterPending: action.status
+            }
+        case 'SET_REGISTER_SUCCESS':
+            return {
+                ...state,
+                isRegisterSuccess: action.status
+            }
+        case 'SET_REGISTER_ERROR':
+            return {
+                ...state,
+                isRegisterError: action.status
+            }
+        default:
+            return state;
+    }
+}
+
+export default RegisterReducer;
