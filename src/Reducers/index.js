a9df1ecd7a083ff72ce13435b5b72bbaf662035a
dean <deanlloyd64@gmail.com>
2020-03-10 02:32:56 -0400

    delete placeholder reducer. add registration status reducer

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import userReducer from './UserReducer'
 import CurrentUserReducer from './CurrentUserReducer'
+import RegistrationsStatusReducer from './RegistrationStatusReducer'
 
 
 export const allReducers = combineReducers({
     userReducer: userReducer,
-    CurrentUserReducer: CurrentUserReducer
+    CurrentUserReducer: CurrentUserReducer,
+    RegistrationsReducer: RegistrationsStatusReducer
 })
