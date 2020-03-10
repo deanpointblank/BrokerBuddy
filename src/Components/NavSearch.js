ccf27e6b90c763aaa2fbe9cf4c08bd2a9295fafb
dean <deanlloyd64@gmail.com>
2020-03-10 03:11:25 -0400

    add state to navsearch to implement smart search

--- src/Components/NavSearch.js
+++ src/Components/NavSearch.js
@@ -3,16 +3,37 @@ import { connect } from 'react-redux'
 import { fetchStocks } from '../Actions/StockActions'
 
 class NavSearch extends Component{
+    constructor(){
+        super();
+        this.state = {
+            search: '',
+            suggestions: []
+        }
+    }
 
 
     componentDidMount(){
         this.props.fetchStocks()
     }
 
+    handleChange(event){
+        this.setState({
+            search: event.target.value
+        })
+        
+    }
+
     render(){
         return(
             <form className="form-inline" autoComplete='off'>
-                <input className="form-control mr-sm-2" type="search" placeholder="Search Stocks" aria-label="Search" />
+                <input
+                    className="form-control mr-sm-2"
+                    type="search"
+                    placeholder="Search Stocks"
+                    aria-label="Search"
+                    value = {this.state.search}
+                    onChange={event => this.handleChange(event)}
+                />
                 <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
             </form>
         )
